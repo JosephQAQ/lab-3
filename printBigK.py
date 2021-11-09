1a63280cef2a4f78959b535d235c5f3318f08539
JosephQAQ <93243892+JosephQAQ@users.noreply.github.com>
2021-11-09 16:05:27 +0800

    Create printBigK.py

--- printBigK.py
+++ printBigK.py
@@ -3,25 +3,21 @@ Created on Oct 13, 201
 @author Leung Cho Tai
 
 print('''
-      k         k
-      k        k
-      k       k 
-      k      k
-      k     k  
-      k    k     
-      k   k
-      k  k 
-      k k
-      kk
-      kk
-      k k
-      k  k
-      k   k
+      K   K
+      K  K 
+      K K
+      KK
+      K K
+      K  K
+      K   K
+      
+      
+      k
+      k
       k    k
-      k     k
-      k      k
-      k       k
-      k        k 
-      k         k
-      k          k
+      k  k
+      kk
+      k  k
+      k    k
+      
 ''')
