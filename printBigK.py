4a311ca85439c5e770f0b578d68c33a315164898
JosephQAQ <93243892+JosephQAQ@users.noreply.github.com>
2021-11-09 15:48:16 +0800

    Add files via upload

---
+++ printBigK.py
@@ -0,0 +1,27 @@
+Created on Oct 13, 201
+
+@author Leung Cho Tai
+
+print('''
+      k         k
+      k        k
+      k       k 
+      k      k
+      k     k  
+      k    k     
+      k   k
+      k  k 
+      k k
+      kk
+      kk
+      k k
+      k  k
+      k   k
+      k    k
+      k     k
+      k      k
+      k       k
+      k        k 
+      k         k
+      k          k
+''')
